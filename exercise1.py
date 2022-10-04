7f4c9d437cc82e50d369cfcaef362a2c67fc285e
Ishaan Desai <ishaandesai@gmail.com>
2022-10-04 14:41:03 +0200

    Solution to task A

--- exercise1.py
+++ exercise1.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 from typing import List
 from math import isclose
+from copy import copy
 
 # Task A: see comment in function `test_vector_index_access`
 # Task B: see comment in function `test_3d_vector_subtraction`
 
 class Vector:
     def __init__(self, coordinates: List[float]) -> None:
-        self._coordinates = coordinates
+        self._coordinates = copy(coordinates)
 
     def __getitem__(self, i: int) -> float:
         return self._coordinates[i]
@@ -32,7 +33,7 @@ def test_vector_index_access() -> None:
         vector[index] = 42.0
         assert vector[index] == 42.0
 
-        # Task A: make this test pass by ensuring that `Vector` uses a copy of the coordinates it receives in the constructor 
+        # Task A: make this test pass by ensuring that `Vector` uses a copy of the coordinates it receives in the constructor
         assert reference[index] != 42.0
 
 
